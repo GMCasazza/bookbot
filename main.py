f8205e9d02e8dfa5d61ffe914ba9a7fb4b01f367
GMCasazza <gregory.casazza@gmail.com>
2025-04-02 16:28:53 -0400

    Completed project - can generate stats from books

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from stats import count_words
 from stats import count_characters
-print (f"{count_words("./books/frankenstein.txt")}", {count_characters("./books/frankenstein.txt")})
+from stats import sorted_list
+import sys
+if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+word_count = count_words(sys.argv[1])
+char_counts = count_characters(sys.argv[1])
+sorted_chars = sorted_list(char_counts)
+
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
+print(f"Found {word_count} total words")
+print("--------- Character Count -------")
+for char_dict in sorted_chars:
+    char = char_dict["char"]
+    count = char_dict["count"]
+    if char.isalpha():
+        print(f"{char}: {count}")
+
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ def count_words (filepath):
         content = file.read()
         words = content.split()
         return (len(words))
-print (f"{count_words("./books/frankenstein.txt")} words found in the document")
+#print (f"{count_words("./books/frankenstein.txt")} words found in the document")
 
 def count_characters(filepath):
     with open(filepath) as file:
@@ -12,13 +12,20 @@ def count_characters(filepath):
     frenquency = {}
     for char in lowercase:
         frenquency[char] = frenquency.get(char, 0) +1
-    print(frenquency)
+    return(frenquency)
 
-#def sorted_list (dictionary):
-#    dictionary = count_characters("./books/frankenstein.txt")
 
-   # dictionary.sort(key=None)
-#    print (dictionary)
-#print(f"{sorted_list("./books/frankenstein.txt")}")
-
+def sorted_list(char_dict):
+    chars_list = []
+    for char, count in char_dict.items():
+        chars_list.append({"char": char, "count": count})
+    
+    
+    def sort_on(dict):
+        return dict["count"]
+    
+    
+    chars_list.sort(reverse=True, key=sort_on)
+    
+    return chars_list
 
